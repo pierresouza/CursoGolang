128807323d6e10e46a2f16ce1a390c38a0f20f02
pierresouza <pierre.s3@hotmail.com>
2024-04-23 22:07:51 -0300

    first commit

---
+++ aula1/hello.go
@@ -0,0 +1,14 @@
+package main // Define o pacote principal
+
+import "fmt" // Importa a biblioteca fmt
+
+func main(){
+
+	fmt.Println("Hello World!") // Imprime na tela a mensagem "Hello World!"
+
+	//fmt = biblioteca
+	// . = chamada de métodos da biblioteca
+	// println = é um método
+	// () = é a execução do método
+	// paramêtros = Valores que são passados para o método
+}
---
+++ variaveis/dados.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//numeros inteiros
+	var idade int
+	idade = 18
+
+	//numeros float
+	var altura float32
+	altura = 1.8
+
+	//string = palavras / cadeias de caracteres
+	var nome string
+	nome = "rafa"
+
+	//booleano True/false
+
+	var booleano bool
+	booleano = true
+
+	fmt.Println(idade)
+	fmt.Println(altura)
+	fmt.Println(nome)
+	fmt.Println(booleano)
+}
